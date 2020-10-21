ccaf9abe57a1189b92b737de324ceb9e84187504
PennWarren <51812919+PennWarren@users.noreply.github.com>
2020-10-21 01:27:36 -0400

    Initial commit --created functions to write last 90 days of data for presidential and senate elections --saving data from 10/21/2020

--- predictit_markets.py
+++ predictit_markets.py
@@ -33,12 +33,14 @@ def market_name(market):
 electoral_college_list = (("Alabama", 6625),
                           ("Alaska", 6591),
                           ("Arizona", 5596),
+                          ("Arkansas", 6597),
                           ("California", 6611),
                           ("Colorado", 5605),
                           ("Connecticut", 6587),
                           ("D.C.", 6644),
+                          ("Delaware", 6636),
                           ("Florida", 5544),
-                          ("Georgia", 6651),
+                          ("Georgia", 5604),
                           ("Hawaii", 6631),
                           ("Idaho", 6623),
                           ("Illinois", 6613),
@@ -64,7 +66,7 @@ electoral_college_list = (("Alabama", 6625),
                           ("Nevada", 5601),
                           ("New Hampshire", 6580),
                           ("New Jersey", 6580),
-                          ("New Mexico", 6752),
+                          ("New Mexico", 6573),
                           ("New York", 6612),
                           ("North Carolina", 5599),
                           ("North Dakota", 6637),
@@ -85,15 +87,51 @@ electoral_college_list = (("Alabama", 6625),
                           ("Wisconsin", 5542),
                           ("Wyoming", 6632))
 
+senate_race_list = (("Alabama", 6183),
+                    ("Alaska", 6688),
+                    ("Arizona (Special)", 5809),
+                    ("Arkansas", 6827),
+                    ("Colorado", 5810),
+                    ("Delaware", 6808),
+                    ("Georgia (Regular)", 6651),
+                    ("Georgia (Special)", 6567),
+                    ("Idaho", 6810),
+                    ("Illinois", 6830),
+                    ("Iowa", 6648),
+                    ("Kansas", 5781),
+                    ("Kentucky", 6575),
+                    ("Louisiana", 6813),
+                    ("Maine", 5811),
+                    ("Massachusetts", 6840),
+                    ("Michigan", 6576),
+                    ("Minnesota", 6761),
+                    ("Mississippi", 6839),
+                    ("Montana", 6568),
+                    ("Nebraska", 6832),
+                    ("New Hampshire", 6795),
+                    ("New Jersey", 6801),
+                    ("New Mexico", 6752),
+                    ("North Carolina", 5808),
+                    ("Oklahoma", 6841),
+                    ("Oregon", 6786),
+                    ("Rhode Island", 6842),
+                    ("South Carolina", 6706),
+                    ("South Dakota", 6826),
+                    ("Tennessee", 6792),
+                    ("Texas", 6788),
+                    ("Virginia", 6821),
+                    ("West Virginia", 6837),
+                    ("Wyoming", 6797))
 
-def get_ninety_days():
-    save_file = 'predictit_presidential' + str(datetime.date.today())
+
+def get_ninety_days_presidential():
+    save_file = 'PredictIt/Presidential/predictit_presidential_' + str(datetime.date.today())
     with open(save_file, 'w+', newline='') as f:
         count = 0
 
         for i, race in enumerate(electoral_college_list):
 
-            time.sleep(2.5)
+            time.sleep(2.25)
             market_data = data(race[0], race[1])
 
             print(i)
@@ -101,3 +139,24 @@ def get_ninety_days():
                 market_data.to_csv(f, header=True)
             else:
                 market_data.to_csv(f, header=False)
+
+def get_ninety_days_senate():
+    save_file = 'PredictIt/Senate/predictit_senate_' + str(datetime.date.today())
+    with open(save_file, 'w+', newline='') as f:
+        count = 0
+
+        for i, race in enumerate(senate_race_list):
+
+            time.sleep(2.25)
+            market_data = data(race[0], race[1])
+
+            print(i)
+            if (i == 0):
+                market_data.to_csv(f, header=True)
+            else:
+                market_data.to_csv(f, header=False)
+
+
+if __name__ == '__main__':
+    get_ninety_days_presidential()
+    get_ninety_days_senate()
